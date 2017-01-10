20117726ec78e8f0e244945baa11e844e9c77309
Raghu Betina <raghu@firstdraft.com>
2017-01-10 21:29:51 +0000

    Direct association between cuisines and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisine
+
   belongs_to :food,
              :class_name => "Venue",
              :foreign_key => "venue_id"
